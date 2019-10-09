f2da2aca3426c1fa2c08c77b03f95f650a7f0b80
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-09 17:44:46 -0400

    Test box component from material-ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Box from '@material-ui/core/Box';
 
 class App extends React.Component {
 
     render(){
-        return ();
+        return (
+            <box>
+                <h1>test</h1>
+            </box>
+        );
     }
 }
 
