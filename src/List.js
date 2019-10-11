3b3036635a83d878023d0f7e4c4ea4533ede064a
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-11 11:02:22 -0400

    Add method to dynamicly render list based on given array

--- src/List.js
+++ src/List.js
@@ -8,11 +8,19 @@ const ListElement = React.lazy(() => import('./ListElement'));
 
 class List extends React.Component {
 
+    renderList(listArray){
+        const length = listArray.length;
+
+        return (listArray.map((element)=>
+            <ListElement value={element}/>
+        ));
+    }
+
     render(){
 
         return (
             <Suspense fallback={<Loading/>}>
-                <ListElement/>
+                {this.renderList(this.props.itens)}
             </Suspense>
         );
     }
